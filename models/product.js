0f3be9bf3fb9ab5a1e14afbd73955da8fe1c8655
Adesewa Aishat <adesewaaishat@gmail.com>
2024-03-16 23:07:11 +0000

    add timestamp to product model

--- models/product.js
+++ models/product.js
@@ -8,5 +8,8 @@ price: {type: Number, required: true},
 images: [{type:String}],
 category: {type:mongoose.Types.ObjectId, ref:'Category'},
 properties: {type:Object},
+},
+{
+  timestamps: true,
 });
 export const Product = models.Product || model('Product', ProductSchema);
